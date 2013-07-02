b8faaafb0934c2c9ac56d9a5a6de32523e4b25cc
Alexander Favaro and Nathan Malkin <github+afavaro+nmalkin@squareup.com>
2013-07-01 18:02:42 -0700

    Add test to make sure ExtractProto serializes properly

--- src/test/java/com/squareup/cascading2/function/ExtractProtoTest.java
+++ src/test/java/com/squareup/cascading2/function/ExtractProtoTest.java
@@ -1,17 +1,25 @@
 package com.squareup.cascading2.function;
 
+import cascading.flow.FlowDef;
+import cascading.flow.hadoop.HadoopFlowConnector;
 import cascading.flow.hadoop.HadoopFlowProcess;
 import cascading.operation.Function;
 import cascading.operation.FunctionCall;
+import cascading.pipe.Each;
 import cascading.pipe.Pipe;
+import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntry;
 import cascading.tuple.TupleEntryCollector;
 import com.squareup.cascading2.generated.Example;
+import com.squareup.cascading2.scheme.ProtobufScheme;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
 import junit.framework.TestCase;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class ExtractProtoTest extends TestCase {
   private static final Example.Partnership P1 = Example.Partnership.newBuilder()
@@ -78,6 +86,30 @@ public class ExtractProtoTest extends TestCase {
   public void testEnum() throws Exception {
     assertEquals(new Tuple("Jack", "jack@", Example.Person.Position.CEO.getNumber()),
         exec(new ExtractProto(Example.Partnership.class, "leader.name", "leader.email", "leader.position"), new Tuple(P3)));
+  }
+
+  public void testSerialization() throws Exception {
+    Pipe p = new Each("input", new ExtractProto(Example.Partnership.class, "leader.name"));
+
+    final String INPUT_PATH = "/tmp/ExtractProtoTest/input";
+    final String OUTPUT_PATH = "/tmp/ExtractProtoTest/output";
+
+    Hfs inputTap = new Hfs(new ProtobufScheme("partnership", Example.Partnership.class),
+        INPUT_PATH);
+    Hfs outputTap = new Hfs(new ProtobufScheme("leader.name", Example.Partnership.class),
+        OUTPUT_PATH);
+
+    // make sure the input path exists
+    inputTap.openForWrite(new HadoopFlowProcess()).close();
+    // make sure the output path does not exist
+    FileSystem.get(new Configuration()).delete(new Path(OUTPUT_PATH), true);
+
+    FlowDef flowDef = new FlowDef()
+        .addSource("input", inputTap)
+        .addSink(p, outputTap)
+        .addTail(p);
+
+    new HadoopFlowConnector().connect(flowDef).complete();
   }
 
   private static Tuple exec(Function f, final Tuple input) {
