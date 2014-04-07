0272a74472f9c0af5f1c839363b9a676329ada19
Sunil Srivatsa <sunil@squareup.com>
2014-04-07 16:02:01 -0700

    add DecodeByteString function/tests and conjar publishing

---
+++ src/main/java/com/squareup/cascading2/function/DecodeByteString.java
@@ -0,0 +1,35 @@
+package com.squareup.cascading2.function;
+
+import cascading.flow.FlowProcess;
+import cascading.operation.BaseOperation;
+import cascading.operation.Function;
+import cascading.operation.FunctionCall;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import com.google.protobuf.ByteString;
+import java.io.UnsupportedEncodingException;
+
+/**
+ *  Convert a ByteString {@link com.google.protobuf.ByteString} to a decoded string of the given encoding.
+ *  Defaults to UTF8 if no encoding is provided.
+ */
+public class DecodeByteString extends BaseOperation implements Function {
+  private final String encoding;
+
+  public DecodeByteString(String charsetName, String fieldName) {
+    super(1, new Fields(fieldName));
+    encoding = charsetName;
+  }
+
+  public DecodeByteString(String fieldName) {
+    this("UTF8", fieldName);
+  }
+
+  @Override public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
+    try {
+      functionCall.getOutputCollector().add(new Tuple(((ByteString) functionCall.getArguments().getObject(0)).toString(encoding)));
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException("Unsupported encoding exception: ", e);
+    }
+  }
+}
---
+++ src/test/java/com/squareup/cascading2/function/DecodeByteStringTest.java
@@ -0,0 +1,54 @@
+package com.squareup.cascading2.function;
+
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import com.google.protobuf.ByteString;
+import com.squareup.cascading_helpers.util.TestHelpers;
+import java.util.Arrays;
+import java.util.List;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class DecodeByteStringTest extends TestCase {
+  @Test
+  public void testByteStringToUTF8() throws Exception {
+    List<Tuple> results = TestHelpers.exec(
+        new DecodeByteString("blah"),
+        new Fields("blah"),
+        new Tuple(ByteString.copyFromUtf8("1337")),
+        new Tuple(ByteString.copyFromUtf8("brian")));
+
+    assertEquals(Arrays.asList(
+        new Tuple("1337"),
+        new Tuple("brian")),
+        results);
+  }
+
+  @Test
+  public void testByteStringToISO88591() throws Exception {
+    List<Tuple> results = TestHelpers.exec(
+        new DecodeByteString("ISO-8859-1", "blah"),
+        new Fields("blah"),
+        new Tuple(ByteString.copyFromUtf8("1337")),
+        new Tuple(ByteString.copyFromUtf8("brian")));
+
+    assertEquals(Arrays.asList(
+        new Tuple("1337"),
+        new Tuple("brian")),
+        results);
+  }
+
+  @Test
+  public void testBadEncoding() throws Exception {
+    try {
+      List<Tuple> results = TestHelpers.exec(
+          new DecodeByteString("LOL I DONT EXIST", "blah"),
+          new Fields("blah"),
+          new Tuple(ByteString.copyFromUtf8("1337")),
+          new Tuple(ByteString.copyFromUtf8("brian")));
+    } catch (RuntimeException e) {
+      return;
+    }
+    fail("did not raise exception");
+ }
+}
